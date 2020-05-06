6758878e7bfdb7d08bb47f9be0ecab4a3e5a3fac
harry <harry@toms-iMac.local>
2020-05-06 19:05:33 +0900

    create, log in, log out for user added

--- imports/api/posts.js
+++ imports/api/posts.js
@@ -1,3 +1,27 @@
+import { Meteor } from "meteor/meteor";
 import { Mongo } from "meteor/mongo";
+import { check } from "meteor/check";
 
-export const Posts = new Meteor.collection("posts");
+export const Posts = new Mongo.collection("posts");
+
+Meteor.methods({
+  "posts.insert"(title, text) {
+    check(title, string);
+    check(text, string);
+
+    Posts.insert({
+      title,
+      text,
+      createdAt: new Date(),
+    });
+  },
+  "posts.delete"(postId) {
+    check(postId, string);
+
+    Posts.remove(postId);
+  },
+  "posts.update"() {},
+  "posts.get"(postId) {
+    check(postId, string);
+  },
+});
--- imports/api/users.js
+++ imports/api/users.js
@@ -1,3 +1,47 @@
-import { Mongo } from "meteor/mongo";
+import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
+import { Accounts } from "meteor/account";
 
-export const Users = new Meteor.collection("users");
+Meteor.methods({
+  "users.insert"(email, password, username, phonenumber) {
+    check(email, string);
+    check(password, string);
+    check(username, string);
+    check(phonenumber, string);
+
+    Accounts.createUser({
+      email,
+      password,
+      username,
+      protile: { phonenumber },
+    });
+  },
+  "users.delete"(userId) {
+    check(userId, string);
+
+    /*if(userId !== this.user.id) {
+        throw new Meteor.Error("Not Authorized")
+      }*/
+
+    Meteor.users.remove(userId);
+  },
+  "users.update"() {},
+  "users.getProfile"(userId) {
+    check(userId, string);
+
+    /*if(userId !== this.user.id) {
+        throw new Meteor.Error("Not Authorized")
+    }*/
+    return Meteor.users.findOne(userId);
+  },
+  "users.passwordLogIn"(user, password) {
+    check(user, string);
+    check(password, string);
+
+    Meteor.loginWithPassword(user, password);
+  },
+
+  "users.logOut"() {
+    Meteor.logout();
+  },
+});
